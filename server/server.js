769bc81d2f1bf0586b8a35b1852d94753c575d83
Schultzy <Schultzy1405@gmail.com>
2024-07-18 18:12:30 -0500

    Final

--- server/server.js
+++ server/server.js
@@ -2,12 +2,43 @@ const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
+const { MongoClient } = require("mongodb");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Connection string to local instance of MongoDB
+const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+// Initialize a new instance of MongoClient
+const client = new MongoClient(connectionStringURI);
+
+// Declare a variable to hold the connection
+let db;
+
+// Create variable to hold our database name
+const dbName = "inventoryDB";
+
+// Use connect method to connect to the mongo server
+client
+  .connect()
+  .then(() => {
+    console.log("Connected successfully to MongoDB");
+    // Use client.db() constructor to add new db instance
+    db = client.db(dbName);
+
+    // Start up express server
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Mongo connection error: ", err.message);
+  });
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
