ebdde5dbc1391dba6dcd753811945105ed183019
Schultzy <Schultzy1405@gmail.com>
2024-07-18 18:26:09 -0500

    hopefully done

--- server/server.js
+++ server/server.js
@@ -27,43 +27,30 @@ client
     // Use client.db() constructor to add new db instance
     db = client.db(dbName);
 
-    // Start up express server
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`);
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.use("/graphql", expressMiddleware(server));
+
+    // if we're in production, serve client/dist as static assets
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, "../client/dist")));
+
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      });
+    }
+
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   })
   .catch((err) => {
     console.error("Mongo connection error: ", err.message);
   });
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-const startApolloServer = async () => {
-  await server.start();
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  app.use("/graphql", expressMiddleware(server));
-
-  // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
-
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
-  }
-
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
-  });
-};
-
-startApolloServer();
