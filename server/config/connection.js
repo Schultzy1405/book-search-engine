823f8fe7bf3cd30e7cec1565175a62028cb5d3d7
Schultzy <Schultzy1405@gmail.com>
2024-07-18 18:40:42 -0500

    reverted back

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
-const { connect, connection } = require("mongoose");
+const mongoose = require("mongoose");
 
-connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+);
 
-module.exports = connection;
+module.exports = mongoose.connection;
