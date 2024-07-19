ee9006a538b624365f4c967daad44fde9e2323aa
Schultzy <Schultzy1405@gmail.com>
2024-07-18 19:01:12 -0500

    just work please

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
 const { MongoClient } = require("mongodb");
-
+require("dotenv").config();
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
